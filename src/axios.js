4d45352309684feaadee4c0605606b734af7492a
Frozanziaee <130326629+Frozanziaee@users.noreply.github.com>
2024-12-14 23:13:03 +0430

    live demo url added

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 const token = localStorage.getItem('token')
 
 const customFetch = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+ // baseURL: 'http://localhost:3000/api/v1',
+  baseUrl:'taskmaster-api-production-d718.up.railway.app/api/v1',
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${token}`
